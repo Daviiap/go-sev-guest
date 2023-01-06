cd2e90d7485a48de75317d11e80e1ce65745984b
Daviiap <davii.sapiens@gmail.com>
2023-01-06 00:31:10 +0000

    refactor: refatorando codigo

--- guest/src/main.go
+++ guest/src/main.go
@@ -3,38 +3,78 @@ package main
 import (
 	"fmt"
 	"os"
-	commands "sev-guest/src/commands"
+	"sev-guest/src/commands"
 )
 
 func printUsage() {
 	fmt.Println("sevtool --[command] [command opts]")
 	fmt.Println("commands:")
-	fmt.Println("	get_report")
-	fmt.Println("		Input params:")
-	fmt.Println("			filename - name of the report binary file")
+	fmt.Println("  --get_report")
+	fmt.Println("    Input params:")
+	fmt.Println("      filename - name of the report binary file")
+}
+
+func getReport(options getReportOptions) {
+	report := commands.AttestationReport{}
+	reportBin, err := commands.GetReport([64]byte{}, &report)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	commands.WriteAttestationReport(&reportBin, options.Filename)
+}
+
+type getReportOptions struct {
+	Filename     string
+	DataFileName string
+}
+
+type readReportOptions struct {
+	Filename string
+}
+
+type commandsOpts struct {
+	PrintUsage        bool
+	GetReport         bool
+	GetReportOptions  getReportOptions
+	ReadReport        bool
+	ReadReportOptions readReportOptions
+}
+
+func isValidIndex(index int, length int) bool {
+	return length > index
+}
+
+func parseOptions(cmdOpts *commandsOpts) {
+	args := os.Args[1:]
+
+	i := 0
+
+	for _, s := range args {
+		switch s {
+		case "--get_report":
+			cmdOpts.GetReport = true
+			cmdOpts.GetReportOptions.Filename = "report.bin"
+
+			if isValidIndex(i+1, len(args)) {
+				cmdOpts.GetReportOptions.Filename = args[i+1]
+			}
+		case "--help":
+			cmdOpts.PrintUsage = true
+		}
+
+		i++
+	}
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) > 0 {
-		switch args[0] {
-		case "--get_report":
-			report := commands.AttestationReport{}
-			bin, err := commands.GetReport([64]byte{}, &report)
+	cmds := commandsOpts{}
+	parseOptions(&cmds)
 
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			if args[1] == "write" {
-				commands.WriteAttestationReport(&bin, "report.bin")
-			} else if args[1] == "read" {
-				commands.PrintAttestationReport(&report)
-			}
-		default:
-			printUsage()
-		}
-	} else {
-		fmt.Println("Command not recognized.")
+	if cmds.PrintUsage {
+		printUsage()
+	} else if cmds.GetReport {
+		getReport(cmds.GetReportOptions)
 	}
 }
--- guest/src/snp/snp_structs.go
+++ guest/src/snp/snp_structs.go
@@ -14,20 +14,6 @@ type SnpGuestRequestIOCtl struct {
 	FWErr      uint64
 }
 
-func getSNPDevFD() (uintptr, error) {
-	file, err := os.Open("/dev/sev-guest")
-
-	if err != nil {
-		return 0, err
-	}
-
-	defer file.Close()
-
-	fd := file.Fd()
-
-	return fd, nil
-}
-
 func SNPIOCtl(guestReq *SnpGuestRequestIOCtl) error {
 	const SNP_GUEST_REQ_IOC_TYPE = 'S'
 	var SNP_GET_REPORT = ioctl.Iowr(uintptr(SNP_GUEST_REQ_IOC_TYPE), 0x0, unsafe.Sizeof(SnpGuestRequestIOCtl{}))
